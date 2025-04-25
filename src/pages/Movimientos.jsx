007c4c22bbe2ffa823c39a9bd633032126df436c
Jhonatan <jurregonieves@gmail.com>
2025-04-24 23:39:11 -0500

    Movimientos.jsx  codigo sobrante que habia comentado obsoleto, eliminado

--- src/pages/Movimientos.jsx
+++ src/pages/Movimientos.jsx
@@ -1,46 +1,4 @@
-// // src/pages/Movimientos.jsx
-// import React, { useEffect, useState } from 'react';
-// import api from '../services/api';
 
-// export default function Movimientos() {
-//   const [movimientos, setMovimientos] = useState([]);
-
-//   const fetchMovimientos = async () => {
-//     const res = await api.get('/movimientos');
-//     setMovimientos(res.data);
-//   };
-
-//   useEffect(() => {
-//     fetchMovimientos();
-//   }, []);
-
-//   return (
-//     <div className="container">
-//       <h3>Movimientos de Inventario</h3>
-//       <table className="table table-bordered">
-//         <thead>
-//           <tr>
-//             <th>Item</th>
-//             <th>Tipo</th>
-//             <th>Cantidad</th>
-//             <th>Fecha</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {movimientos.map(mov => (
-//             <tr key={mov.id}>
-//               <td>{mov.item_nombre}</td>
-//               <td>{mov.tipo_movimiento}</td>
-//               <td>{mov.cantidad}</td>
-//               <td>{new Date(mov.fecha_movimiento).toLocaleString()}</td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// }
-// src/pages/Movimientos.jsx
 import React, { useEffect, useState } from 'react';
 import api from '../services/api';
 import '../styles/tables.css'
